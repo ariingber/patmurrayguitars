aefbe4002e2f1591956a3b885af384f8a81eea37
Ari Ingber <ariingber@gmail.com>
2016-08-19 16:07:50 -0400

    first commit

--- js/init.js
+++ js/init.js
@@ -3,5 +3,29 @@
 
     $('.button-collapse').sideNav();
 
-  }); // end of document ready
+  });
+  // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+$(".button-collapse").sideNav();
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+$(document).ready(function(){
+    $('.slider').slider({full_width: true, height: 600});
+  });
+
+$(document).ready(function() {
+   $('#nav').localScroll({duration:5000});
+});
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
